5cb9b64b9cd31e4bbe0cd9e7433d385cbeab4eef
Ilian Andrianov <ilian.and.04@gmail.com>
2020-05-06 02:38:41 +0300

    added help text removed question of where load data

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from RowSpanTableWidget import RowSpanTableWidget
 from tableparser import Parser
 
 
-from_cache = ''
-while from_cache not in ('y', 'n'):
-    from_cache = input('Load from cache? (y/n) ').lower()
-
-print('loading data...')
-if from_cache == 'y':
-    print('loading from cache...')
-    parser = Parser.from_cache()
-else:
-    print('loading from server...')
+is_loaded = False
+if Parser.is_cache_exists():
+    try:
+        print('Loading from cache...')
+        parser = Parser.from_cache()
+        print('Loaded')
+        is_loaded = True
+    except Exception as ex:
+        print(ex)
+if not is_loaded:
+    print('Loading from server...')
     parser = Parser.from_server()
+    print('Loaded')
+    is_loaded = True
     parser.save_cache()
-print('loaded')
 
 
 class Ui_MainWindow(object):
@@ -38,7 +40,7 @@ class Ui_MainWindow(object):
         self.table.setGeometry(QtCore.QRect(20, 80, 481, 481))
         self.table.setObjectName("table")
         self.table.horizontalHeader().setSectionResizeMode(
-                QtWidgets.QHeaderView.Stretch)
+            QtWidgets.QHeaderView.Stretch)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 528, 20))
@@ -73,17 +75,17 @@ class Ui_MainWindow(object):
                                                     "Input your name here"))
 
     def update_table(self):
-        name = self.lineEdit.text()
+        name = self.lineEdit.text().lower()
         names = parser.get_names()
         self.table.clear()
         good_names = []
         for el in names:
-            if len(el) >= len(name) and el[:len(name)] == name:
+            if len(el) >= len(name) and el[:len(name)].lower() == name:
                 good_names.append(el)
                 self.table.appendRow([3], el)
         if not good_names:
             for el in names:
-                if name in el:
+                if name in el.lower():
                     good_names.append(el)
                     self.table.appendRow([3], el)
         if len(good_names) == 1:
@@ -92,13 +94,33 @@ class Ui_MainWindow(object):
             self.table.appendRow([1, 1, 1], ["Contest id", "Problem", "Score"])
             for el in stat:
                 self.table.appendRow(
-                        [1, 1, 1],
-                        map(str, [el.contest.id, el.short_name, el.score])
-                        )
+                    [1, 1, 1],
+                    map(str, [el.contest.id, el.short_name, el.score])
+                )
 
     def open_help(self):
+        text_help = '''Problem Chooser for 22b class, school 179
+
+Здесь вы можете узнать, какие задачи вам будет проще всего решить, что бы \
+побыстрее закрыть дедлайн.
+
+В поле ввода можно ввести своё имя, и вам будет предложен список \
+нерешённых задач в порядке возрастания сложности.
+В списке вы можете увидеть счёт задачи, id контеста и название задачи (как в \
+тестирующей системе - A, B, C и т.д.)
+
+Score - счёт задач. Чем больше счёт, тем, скорее всего, вам будет проще \
+решить задачу. Это значение зависит от посылок других людей, так что у самых \
+новых задач счёт будет не совсем корректен (он просто будет около нуля).
+
+Данные о посылках для подсчёта простоты задач берутся из общей таблицы \
+ejudge, так что если server.179.ru недоступен, то обновить данные не \
+получиться. Но они сохраняются, так что при перезапуске программы все данные \
+останутся.
+Вручную обновить данные можно при нажатии Сtrl+R или в меню.
+'''
         QtWidgets.QMessageBox.about(self.centralwidget, "Help",
-                                    "Текст-заглушка")
+                                    text_help)
 
     def reload_table(self):
         global parser
