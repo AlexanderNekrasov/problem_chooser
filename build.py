dbc08bc828bfe248b90315ff77cba07bab4f1498
Ilian Andrianov <ilian.and.04@gmail.com>
2020-05-16 21:37:57 +0300

    refactoring build.py

--- build.py
+++ build.py
@@ -46,7 +46,8 @@ if os.path.exists('main.spec'):
 
 print("\nBUILDING...")
 exit_code = os.system(
-    sys.executable + ' -m PyInstaller -F main.py --clean --noconsole --add-data resources/help:reru sources')
+    sys.executable + ' -m PyInstaller -F main.py --clean --noconsole \
+                      --add-data resources/help:reru sources')
 if exit_code:
     print("\nBUILDING FAILED")
     sys.exit(exit_code)
